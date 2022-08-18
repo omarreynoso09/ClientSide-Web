fae149cbfbe643414bb648b7d6f0a4680a5d61b7
Hansel Reynoso <omarreynoso09@gmail.com>
2022-08-18 12:59:51 -0400

    added 404 page changes

--- src/pages/Error404.js
+++ src/pages/Error404.js
@@ -1,9 +1,26 @@
-import React from "react";
+import * as React from "react";
+import Stack from "@mui/material/Stack";
 
 export default function Error404() {
   return (
-    <div>
-      <h2>Error 404...</h2>
-    </div>
+    <>
+      <br></br>
+      <h1> 404 page</h1>
+      <h3> This page could not be found click the next link</h3>
+      <br></br>
+      <Stack direction="row" spacing={2}></Stack>
+    </>
   );
 }
+
+// export default function Error404() {
+//   return (
+//     <div id="wrapper">
+
+//       <button className="button1" link to="/">
+//         {" "}
+//         Go Back Home
+//       </button>
+//     </div>
+//   );
+// }
